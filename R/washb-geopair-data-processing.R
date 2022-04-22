c35bb3603c36c971fb50fcc19e0a3309d81244dc
Ben Arnold <bfarnold@gmail.com>
2022-04-22 08:40:14 -0700

    Update data processing to read and write data from Box

--- R/washb-geopair-data-processing.R
+++ R/washb-geopair-data-processing.R
@@ -14,38 +14,40 @@ library(here)
 # source configuration file
 source(here("R","washb-geopair-Config.R"))
 
+# file path to the Box data directory
+Box_data_directory <- "~/Library/CloudStorage/Box-Box/washb-geopair/data/"
 
 #----------------------------------
 # Bangladesh public data
 #----------------------------------
 
 # cluster level treatment assignments
-d_tr <- read_csv(here("data","washb-bangladesh-tr-public.csv"))
+d_tr <- read_csv(file=paste0(Box_data_directory,"untouched/washb-bangladesh-tr-public.csv"))
 
 # child anthropometry measured at
 # follow-up visits 1 and 2
-d_anth <- read_csv(here("data","washb-bangladesh-anthro-public.csv"))
+d_anth <- read_csv(file=paste0(Box_data_directory,"untouched/washb-bangladesh-anthro-public.csv"))
 
 
 # child development data
 # at follow-up visit 2
 # Extended Ages and Stages Questionnaire (EASQ)
 # and MacArthur-Bates Communicative Development Inventory (CDI)
-d_easq <- read_csv(here("data","washb-bangladesh-easq-year2.csv"))
-d_cdi  <- read_csv(here("data","washb-bangladesh-cdi-year2.csv"))
+d_easq <- read_csv(file=paste0(Box_data_directory,"untouched/washb-bangladesh-easq-year2.csv"))
+d_cdi  <- read_csv(file=paste0(Box_data_directory,"untouched/washb-bangladesh-cdi-year2.csv"))
 
 # child diarrhea measured at 
 # baseline and at 
 # follow-up visits 1 and 2
-d_diar <- read_csv(here("data","washb-bangladesh-diar-public.csv"))
+d_diar <- read_csv(file=paste0(Box_data_directory,"untouched/washb-bangladesh-diar-public.csv"))
 
 # child protozoan infection measured
 # at follow-up visit 2
-d_prot <- read_csv(here("data","washb-bangladesh-protozoa-public.csv"))
+d_prot <- read_csv(file=paste0(Box_data_directory,"untouched/washb-bangladesh-protozoa-public.csv"))
 
 # child helminth infection measured
 # at follow-up visit 2
-d_sth <- read_csv(here("data","washb-bangladesh-sth-public.csv"))
+d_sth <- read_csv(file=paste0(Box_data_directory,"untouched/washb-bangladesh-sth-public.csv"))
 
 #----------------------------------
 # merge on treatment assignments
@@ -60,16 +62,16 @@ d_sth <- read_csv(here("data","washb-bangladesh-sth-public.csv"))
 # limit to relevant variables
 #----------------------------------
 d_anth2 <- d_anth %>%
-  select(dataid,childid,tchild,clusterid,svy,waz,laz,whz,hcz) %>%
+  dplyr::select(dataid,childid,tchild,clusterid,svy,waz,laz,whz,hcz) %>%
   left_join(d_tr, by = "clusterid") %>%
   filter(svy==2) %>%
   mutate(tr = factor(tr, levels = c("Control","Water","Sanitation","Handwashing","WSH","Nutrition","Nutrition + WSH"))
   ) %>%
-  select(-svy)
+  dplyr::select(-svy)
   
 
 d_easq2 <- d_easq %>%
-  select(dataid,childid,clusterid,block,tchild,arm, z_easq_com = z_com, z_easq_motor = z_motor, z_easq_pers = z_personal, z_easq_total = z_combined) %>%
+  dplyr::select(dataid,childid,clusterid,block,tchild,arm, z_easq_com = z_com, z_easq_motor = z_motor, z_easq_pers = z_personal, z_easq_total = z_combined) %>%
   mutate(tr = case_when(
     arm == 1 ~ "Sanitation",
     arm == 2 ~ "Handwashing",
@@ -81,11 +83,11 @@ d_easq2 <- d_easq %>%
   ), 
   tr = factor(tr, levels = c("Control","Water","Sanitation","Handwashing","WSH","Nutrition","Nutrition + WSH"))
   ) %>%
-  select(-arm)
+  dplyr::select(-arm)
   
 
 d_cdi2 <- d_cdi %>%
-  select(dataid,childid,clusterid,block,tchild,arm, z_cdi_comp = z_endline_CDI_understand, z_cdi_expr = z_endline_CDI_say) %>%
+  dplyr::select(dataid,childid,clusterid,block,tchild,arm, z_cdi_comp = z_endline_CDI_understand, z_cdi_expr = z_endline_CDI_say) %>%
   mutate(tr = case_when(
     arm == 1 ~ "Sanitation",
     arm == 2 ~ "Handwashing",
@@ -97,30 +99,30 @@ d_cdi2 <- d_cdi %>%
   ), 
   tr = factor(tr, levels = c("Control","Water","Sanitation","Handwashing","WSH","Nutrition","Nutrition + WSH"))
   ) %>%
-  select(-arm)
+  dplyr::select(-arm)
   
 
 d_chd2 <- full_join(d_easq2, d_cdi2, by = c("dataid","childid","clusterid","block","tchild","tr")) %>%
-  select(dataid,childid,clusterid,tchild,block,tr,everything())
+  dplyr::select(dataid,childid,clusterid,tchild,block,tr,everything())
 
 d_diar2 <- d_diar %>%
-  select(dataid,childid,tchild,clusterid,svy,diar7d) %>%
+  dplyr::select(dataid,childid,tchild,clusterid,svy,diar7d) %>%
   left_join(d_tr, by = "clusterid") %>%
   filter(svy > 0) %>%
   mutate(tr = factor(tr, levels = c("Control","Water","Sanitation","Handwashing","WSH","Nutrition","Nutrition + WSH"))
   ) %>%
-  select(-svy)
+  dplyr::select(-svy)
   
 
 d_prot2 <- d_prot %>%
-  select(dataid,clusterid,personid,block,tr,giar=posgi, delta_prot) %>%
+  dplyr::select(dataid,clusterid,personid,block,tr,giar=posgi, delta_prot) %>%
   # exclude missing values
   filter(delta_prot == "Observed") %>%
   mutate(tr = factor(tr, levels = c("Control","Water","Sanitation","Handwashing","WSH","Nutrition","Nutrition + WSH"))
   )
 
 d_sth2 <- d_sth %>%
-  select(dataid,clusterid,personid,block,tr,al,tt,hw,sth) %>%
+  dplyr::select(dataid,clusterid,personid,block,tr,al,tt,hw,sth) %>%
   # exclude missing values
   filter(!is.na(al)) %>%
   mutate(tr = factor(tr, levels = c("Control","Water","Sanitation","Handwashing","WSH","Nutrition","Nutrition + WSH"))
@@ -135,10 +137,10 @@ d_parasite <- d_sth2 %>%
 #----------------------------------
 # save analysis files
 #----------------------------------
-write_rds(d_anth2,path = here("data","bangl_analysis_anthro.rds"))
-write_rds(d_chd2,path = here("data","bangl_analysis_chdev.rds"))
-write_rds(d_diar2,path = here("data","bangl_analysis_diar.rds"))
-write_rds(d_parasite,path = here("data","bangl_analysis_parasite.rds"))
+write_rds(d_anth2,file = paste0(Box_data_directory,"final/bangl_analysis_anthro.rds"))
+write_rds(d_chd2, file = paste0(Box_data_directory,"final/bangl_analysis_chdev.rds"))
+write_rds(d_diar2,file = paste0(Box_data_directory,"final/bangl_analysis_diar.rds"))
+write_rds(d_parasite,file = paste0(Box_data_directory,"final/bangl_analysis_parasite.rds"))
 
 
 #----------------------------------
@@ -146,16 +148,16 @@ write_rds(d_parasite,path = here("data","bangl_analysis_parasite.rds"))
 #----------------------------------
 
 # child anthropometry measured at visit 2
-dk_anth <- read_csv(here("data","washb-kenya-endline-anthro-public.csv")) 
+dk_anth <- read_csv(file=paste0(Box_data_directory,"untouched/washb-kenya-endline-anthro-public.csv")) 
 
 # child development measures
 # at visit 2
-dk_chd <- read_csv(here("data","washk_dev_public_20180201.csv"))
+dk_chd <- read_csv(file=paste0(Box_data_directory,"untouched/washk_dev_public_20180201.csv"))
 
 
 # child diarrhea measured at baseline
 # and at visits 1 and 2
-dk_diar <- read_csv(here("data","washb-kenya-diar-public.csv"))
+dk_diar <- read_csv(file=paste0(Box_data_directory,"untouched/washb-kenya-diar-public.csv"))
 
 
 # child giardia and STH infection
@@ -164,7 +166,7 @@ dk_diar <- read_csv(here("data","washb-kenya-diar-public.csv"))
 # documented as other data
 # use the replication script as
 # documentation: https://osf.io/fpxms/
-dk_para <- haven::read_dta(here("data","parasites_kenya_public_ca20201202.dta"))
+dk_para <- haven::read_dta(file=paste0(Box_data_directory,"untouched/parasites_kenya_public_ca20201202.dta"))
 
 
 #----------------------------------
@@ -175,18 +177,16 @@ dk_para <- haven::read_dta(here("data","parasites_kenya_public_ca20201202.dta"))
 # limit to relevant variables
 #----------------------------------
 dk_anth2 <- dk_anth %>%
-  select(clusterid, compoundid, hhid, childid, block, tr, targetchild, laz=haz, waz, whz, hcz, haz_who, waz_who, whz_who)
+  dplyr::dplyr::select(clusterid, compoundid, hhid, childid, block, tr, targetchild, laz=haz, waz, whz, hcz, haz_who, waz_who, whz_who)
 
 dk_chd2 <- dk_chd %>%
-  select(childidr2, hhidr2, clusteridr2, block, tr, z_easq_com = comtotz, z_easq_motor = mottotz, z_easq_pers = pstotz, z_easq_total = globaltotz)
-
+  dplyr::select(childidr2, hhidr2, clusteridr2, block, tr, z_easq_com = comtotz, z_easq_motor = mottotz, z_easq_pers = pstotz, z_easq_total = globaltotz)
 
 dk_diar2 <- dk_diar %>%
   filter(time > 0) %>%
-  select(clusterid, compoundid, hhid, childid, block, tr, targetchild, diar7d=diarr7) %>%
+  dplyr::select(clusterid, compoundid, hhid, childid, block, tr, targetchild, diar7d=diarr7) %>%
   mutate(childidr2 = (childid+3252)*10,
          clusteridr2 = (clusterid+3252)*10)
-
 
 dk_para2 <- dk_para %>%
   mutate(tr2 = case_when(
@@ -200,13 +200,13 @@ dk_para2 <- dk_para %>%
   ),
   tr2 = factor(tr2, levels = c("Control","Water","Sanitation","Handwashing","WSH","Nutrition","Nutrition + WSH"))
          ) %>%
-  select(childidr2, hhidr2, clusteridr2, block,tr=tr2, giar = giardia_yn, al = ascaris_yn, tt = trichuris_yn, hw = hook_yn, sth = sth_yn)
+  dplyr::select(childidr2, hhidr2, clusteridr2, block,tr=tr2, giar = giardia_yn, al = ascaris_yn, tt = trichuris_yn, hw = hook_yn, sth = sth_yn)
 
 #----------------------------------
 # save analysis files
 #----------------------------------
-write_rds(dk_anth2,path = here("data","kenya_analysis_anthro.rds"))
-write_rds(dk_chd2,path = here("data","kenya_analysis_chdev.rds"))
-write_rds(dk_diar2,path = here("data","kenya_analysis_diar.rds"))
-write_rds(dk_para2,path = here("data","kenya_analysis_parasite.rds"))
+write_rds(dk_anth2,file = paste0(Box_data_directory,"final/kenya_analysis_anthro.rds"))
+write_rds(dk_chd2, file = paste0(Box_data_directory,"final/kenya_analysis_chdev.rds"))
+write_rds(dk_diar2,file = paste0(Box_data_directory,"final/kenya_analysis_diar.rds"))
+write_rds(dk_para2,file = paste0(Box_data_directory,"final/kenya_analysis_parasite.rds"))
 
